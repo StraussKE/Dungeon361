2d8008df84a21664ec5462042c47e77cade46abf
Katie Strauss <selicatiren@gmail.com>
2021-11-15 23:47:59 -0800

    dungeon map buttons toggle on based on currently/previously explored rooms, restricting access to areas that cannot yet be explored

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -27,6 +27,8 @@ namespace MatchGame
 
         const string PLAYER = "🧙‍";
 
+        int [] playerLoc = new int[2]; // col, row
+
         Random random = new Random();
 
         public MainWindow()
@@ -48,7 +50,6 @@ namespace MatchGame
             };
 
             // string death = "☠";
-            
 
             for (int col = 0; col < MAX_COL; col++)
             {
@@ -70,12 +71,16 @@ namespace MatchGame
                     _ = map.Children.Add(roomWalls);
 
                     Button roomContents = new Button();
-                    int flat = col + row + col*row;
+                    int flat = Flatten(col, row);
                     if (flat == 0)
                     {
                         roomContents.Tag = "show";
                         roomContents.Content = PLAYER;
                         roomContents.Foreground = new SolidColorBrush(Colors.Blue);
+                        roomContents.Click += new RoutedEventHandler(EnterRoom_Click);
+                        playerLoc[0] = col;
+                        playerLoc[1] = row;
+                        roomContents.IsEnabled = true;
                     }
                     // monster gen
                     else
@@ -93,8 +98,11 @@ namespace MatchGame
                             roomContents.Tag = resources[selection];
                             roomContents.Foreground = new SolidColorBrush(Colors.Green);
                         }
-                        roomContents.Content = "?";
+                        //roomContents.Content = "?";
+                        roomContents.Content = "" + flat;
+                        roomContents.IsEnabled = false;
                     }
+                    roomContents.Name = "room" + flat.ToString();
                     roomContents.FontSize = 30;
                     roomContents.Height = 76;
                     roomContents.Width = 76;
@@ -103,10 +111,15 @@ namespace MatchGame
                     roomContents.BorderThickness = new Thickness(0);
                     roomContents.Background = new SolidColorBrush(Colors.Tan);
                     roomContents.Click += new RoutedEventHandler(EnterRoom_Click);
+
+                    
                     _ = map.Children.Add(roomContents);
                 }
             }
 
+            OpenRooms(playerLoc[0], playerLoc[1]);
+
+            LookItUp.Click += new RoutedEventHandler(LookUp_Click);
             InfoBox.Text = "Welcome to the Dungeon!!!";
             InfoBox.Foreground = new SolidColorBrush(Colors.Brown);
             InfoBox.FontSize = 50;
@@ -118,9 +131,17 @@ namespace MatchGame
             LookItUp.SetValue(Grid.ColumnProperty, MAX_COL);
         }
 
+        private int Flatten(int col, int row)
+        {
+            return col + row * MAX_COL;
+        }
+
         private void EnterRoom_Click(object sender, RoutedEventArgs e)
         {
             Button room = sender as Button;
+            int row = (int)room.GetValue(Grid.RowProperty);
+            int col = (int)room.GetValue(Grid.ColumnProperty);
+            OpenRooms(col, row);
 
             Dictionary<string, string> symbolNamesDict = new Dictionary<string, string>()
             {
@@ -154,7 +175,6 @@ namespace MatchGame
                 LookUpText.Text = "Click to look up the meaning of " + dictLookup;
                 LookItUp.Tag = dictLookup;
                 LookItUp.BorderThickness = new Thickness(2);
-                LookItUp.Click += new RoutedEventHandler(LookUp_Click);
             }
             InfoBox.Text = inTheRoom;
             InfoBox.FontSize = 20;
@@ -167,48 +187,76 @@ namespace MatchGame
             InfoBox.Text = definition;
         }
 
-        private string CheckDict(string searchTerm)
-        {
+        private string CheckDict(string searchTerm) {
             string pyPath = @"C:\Python39\python.exe";
-
             string workingDirectory = @"D:\GitRepos\C#\Learning Gui\Matchgame\MatchGame\";
-
             string serviceLocation = "DictScrape\\dictionaryService.py";
-            
             string outfileName = "request.txt";
-
             string infileName = searchTerm + ".json";
 
             WriteTxt(workingDirectory + outfileName, searchTerm);
-            ProcessStartInfo lookupStartInfo = new ProcessStartInfo()
-            {
+            ProcessStartInfo lookupStartInfo = new ProcessStartInfo() {
                 WorkingDirectory = workingDirectory,
                 FileName = pyPath,
                 Arguments = serviceLocation,
-                //RedirectStandardOutput = true,
-                //UseShellExecute = false,
-                // CreateNoWindow = true
+                CreateNoWindow = true
             };
 
-            string defined = "";
+            string defined;
 
             Process lookup = Process.Start(lookupStartInfo);
-            if (lookup != null)
-            {
+            if (lookup != null) {
                 lookup.WaitForExit();
                 defined = File.ReadAllText(workingDirectory + infileName);
-            }
-            else
-            {
+            } else {
                 defined = "An Error Occurred";
             }
-            
             return defined;
         }
 
         private void WriteTxt(string filepath, string word)
         {
             File.WriteAllText(filepath, word);
+        }
+
+        private void OpenRooms(int col, int row)
+        {
+            List<Button> openlist = new List<Button>();
+            if(col != 0)
+            {
+                int westCoord = Flatten(col - 1, row);
+                string roomName = "room" + westCoord;
+                Button west = map.Children.OfType<Button>().FirstOrDefault(btn => btn.Name == roomName);
+                openlist.Add(west);
+            }
+            if (col != MAX_COL - 1)
+            {
+                int eastCoord = Flatten(col + 1, row);
+                string roomName = "room" + eastCoord;
+                Button east = map.Children.OfType<Button>().FirstOrDefault(btn => btn.Name == roomName);
+                openlist.Add(east);
+            }
+            if (row != 0)
+            {
+                int northCoord = Flatten(col, row - 1);
+                string roomName = "room" + northCoord;
+                Button north = map.Children.OfType<Button>().FirstOrDefault(btn => btn.Name == roomName);
+                openlist.Add(north);
+            }
+            if (row != MAX_ROW - 1)
+            {
+                int southCoord = Flatten(col, row + 1);
+                string roomName = "room" + southCoord;
+                Button south = map.Children.OfType<Button>().FirstOrDefault(btn=> btn.Name == roomName);
+                openlist.Add(south);
+            }
+            foreach(Button room in openlist)
+            {
+                if(!room.IsEnabled)
+                {
+                    room.IsEnabled = true;
+                }
+            }
         }
     }
 }
